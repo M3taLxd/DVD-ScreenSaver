2250c563cbe36e7892a6445fe33b88825c5faf45
M3taL 360 <rareoscar360@outlook.com>
2020-04-27 17:36:04 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace BouncingBall
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Ball = new System.Windows.Forms.PictureBox();
+            this.BallStepLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.OrangeRed;
+            this.Ball.Image = global::BouncingBall.Properties.Resources.yse_691;
+            this.Ball.Location = new System.Drawing.Point(243, 128);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(132, 134);
+            this.Ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ball.TabIndex = 0;
+            this.Ball.TabStop = false;
+            // 
+            // BallStepLabel
+            // 
+            this.BallStepLabel.AutoSize = true;
+            this.BallStepLabel.BackColor = System.Drawing.Color.White;
+            this.BallStepLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(186)));
+            this.BallStepLabel.Location = new System.Drawing.Point(12, 9);
+            this.BallStepLabel.Name = "BallStepLabel";
+            this.BallStepLabel.Size = new System.Drawing.Size(109, 39);
+            this.BallStepLabel.TabIndex = 1;
+            this.BallStepLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BallStepLabel);
+            this.Controls.Add(this.Ball);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Ball;
+        private System.Windows.Forms.Label BallStepLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBall
+{
+    public partial class Form1 : Form
+    {
+        int horVelocity = 0;
+        int verVelocity = 0;
+        int ballStep = 3;
+
+        Timer mainTimer = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeApp();
+        }
+
+        private void InitializeApp()
+        {
+            verVelocity = ballStep;
+            horVelocity = ballStep;
+
+            this.KeyDown += new KeyEventHandler(App_KeyDown);
+
+            UpdateBallStepLabel();
+            InitializeMainTimer();
+        }
+
+        private void InitializeMainTimer()
+        {
+            mainTimer = new Timer();
+            mainTimer.Tick += new EventHandler(MainTimer_Tick);
+            mainTimer.Interval = 20;
+            mainTimer.Start();
+        }
+
+        private void MainTimer_Tick(object sender, EventArgs e)
+        {
+            MoveBall();
+            BallBorderCollide();
+        }
+
+        private void MoveBall()
+        {
+            Ball.Top += verVelocity;
+            Ball.Left += horVelocity;
+        }
+
+        private void BallBorderCollide()
+        {
+            if (Ball.Top + Ball.Height > ClientRectangle.Height) //collide with bottom border
+            {
+                verVelocity *= -1;
+                //verVelocity = -verVelocity;                
+            }
+            else if (Ball.Top < 0) //collide with top border
+            {
+                verVelocity *= -1;
+                //verVelocity = -verVelocity;
+            }
+            else if (Ball.Left < 0) //collide with left border
+            {
+                horVelocity *= -1;
+                //horVelocity = -horVelocity;
+            }
+            else if (Ball.Left + Ball.Width > ClientRectangle.Width) //collide with right border
+            {
+                horVelocity *= -1;
+                //horVelocity = -horVelocity;
+            }
+        }
+
+        private void App_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.X)
+            {
+                ballStep += 1;
+                verVelocity = ballStep * (verVelocity / Math.Abs(verVelocity));
+                horVelocity = ballStep * (horVelocity / Math.Abs(horVelocity));
+                UpdateBallStepLabel();
+            }
+            else if (e.KeyCode == Keys.Z)
+            {
+                if (ballStep > 0)
+                {
+                    ballStep -= 1;
+                    verVelocity = ballStep * (verVelocity / Math.Abs(verVelocity));
+                    horVelocity = ballStep * (horVelocity / Math.Abs(horVelocity));
+                   
+                }
+                UpdateBallStepLabel();
+            }
+        }
+
+        private void UpdateBallStepLabel()
+        {
+            BallStepLabel.Text = "Ball Step: " + ballStep;
+        }
+
+
+
+    }
+}
